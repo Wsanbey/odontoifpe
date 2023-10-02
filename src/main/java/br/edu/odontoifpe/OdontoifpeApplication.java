54ac9599caba5e7351a6328f73ecf41090b5d43b
Wsanbey <61056410+Wsanbey@users.noreply.github.com>
2023-10-02 13:52:38 -0300

    git start

---
+++ src/main/java/br/edu/odontoifpe/OdontoifpeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.odontoifpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontoifpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontoifpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/odontoifpe/OdontoifpeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.odontoifpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoifpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
